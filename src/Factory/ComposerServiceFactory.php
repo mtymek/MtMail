24c6d943a737548411f53acf44940312f1f4d657
Mateusz Tymek <mtymek@gmail.com>
2014-08-08 13:13:04 +0200

    Removed deprecated code

--- src/Factory/ComposerServiceFactory.php
+++ src/Factory/ComposerServiceFactory.php
@@ -30,28 +30,14 @@ class ComposerServiceFactory implements FactoryInterface
         $renderer = $serviceLocator->get($configuration['mt_mail']['renderer']);
         $service = new Composer($renderer);
 
-        $enabledPlugins = array();
-
-        // trigger E_DEPRECATED if old configuration key is used
-        // TODO: remove this in 1.0-stable version
-        if (isset($configuration['mt_mail']['plugins']) && is_array($configuration['mt_mail']['plugins'])) {
-            trigger_error(
-                "'plugins' configuration key is now deprecated and should be replaced with 'composer_plugins'",
-                E_USER_DEPRECATED
-            );
-            $enabledPlugins = $configuration['mt_mail']['plugins'];
-        }
+        $pluginManager = $serviceLocator->get('MtMail\Service\ComposerPluginManager');
 
         if (isset($configuration['mt_mail']['composer_plugins'])
             && is_array($configuration['mt_mail']['composer_plugins'])
         ) {
-            $enabledPlugins = array_merge($enabledPlugins, $configuration['mt_mail']['composer_plugins']);
-        }
-
-        $pluginManager = $serviceLocator->get('MtMail\Service\ComposerPluginManager');
-
-        foreach ($enabledPlugins as $plugin) {
-            $service->getEventManager()->attachAggregate($pluginManager->get($plugin));
+            foreach ($configuration['mt_mail']['composer_plugins'] as $plugin) {
+                $service->getEventManager()->attachAggregate($pluginManager->get($plugin));
+            }
         }
 
         return $service;
